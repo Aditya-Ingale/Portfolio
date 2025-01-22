1cfd33cc68986254d5da01d187b9603b4b707235
Aditya-Ingale <adityaa.ingale@gmail.com>
2025-01-23 02:17:55 +0530

    Add Code for navbar

--- js/script.js
+++ js/script.js
@@ -1,5 +1,24 @@
-/* ====================== Sticky navbar ===================== */
+/* ====================== Scroll section active link ===================== */
+let sections = document.querySelectorAll('section');
+let navlinks = document.querySelectorAll('header nav a');
+
 window.onscroll = () => {
+    sections.forEach(sec => {
+        let top = window.scrollY;
+        let offset = sec.offsetTop - 150;
+        let height = sec.offsetHeight;
+        let id = sec.getAttribute('id');
+
+        if(top >= offset && top < offset + height) {
+            navlinks.forEach(links => {
+                links.classList.remove('active');
+                document.querySelector('header nav a[href*=' + id + ']').classList.add('active');
+            });
+        };
+    });
+
+
+/* ====================== Sticky navbar ===================== */
 let header = document.querySelector('.header');
 
 header.classList.toggle('sticky', window.scrollY > 100);
