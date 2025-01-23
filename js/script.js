3f3b7eb072b4b2cd34daaa83aec77a721fd4f4af
Aditya-Ingale <adityaa.ingale@gmail.com>
2025-01-24 01:03:37 +0530

    Updated

--- js/script.js
+++ js/script.js
@@ -1,3 +1,13 @@
+/* ====================== Menu icon navbar ===================== */
+let menuIcon = document.querySelector('#menu-icon');
+let navbar = document.querySelector('.navbar');
+
+menuIcon.onclick = () => {
+    menuIcon.classList.toggle('bx-x');
+    navbar.classList.toggle('active');
+};
+
+
 /* ====================== Scroll section active link ===================== */
 let sections = document.querySelectorAll('section');
 let navlinks = document.querySelectorAll('header nav a');
@@ -22,4 +32,42 @@ window.onscroll = () => {
 let header = document.querySelector('.header');
 
 header.classList.toggle('sticky', window.scrollY > 100);
+
+
+/* ====================== remove menu icon navbar when click navbar link (scroll) ===================== */
+menuIcon.classList.remove('bx-x');
+navbar.classList.remove('active');
 };
+
+/* ====================== dark light ===================== */
+let darkModeIcon = document.querySelector('#darkMode-icon');
+
+darkModeIcon.onclick = () => {
+    darkModeIcon.classList.toggle('bx-sun');
+    document.body.classList.toggle('dark-mode');
+};
+
+
+/*=====scroll reveal======*/
+ScrollReveal({
+    //reset:true,
+    distance: '80px',
+    duration: 2000,
+    delay: 200,
+  });
+  
+  ScrollReveal().reveal('.home-content, .heading', {
+    origin: 'top'
+  });
+  
+  ScrollReveal().reveal('.home-img img, .educations-container, .portfolio-container, .contact form', {
+    origin: 'bottom'
+  });
+  
+  ScrollReveal().reveal('.home-content h1, .about-img img, .experience-img img', {
+    origin: 'left'
+  });
+  
+  ScrollReveal().reveal('.home-content h3, .home-content p, .about-content, .experience-content', {
+    origin: 'right'
+  });
